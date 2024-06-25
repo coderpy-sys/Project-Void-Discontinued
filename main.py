9422b03f09fcbe3d979a5394a3511c27d3629f2b
vvyn-dev <vienii@proton.me>
2024-06-25 14:59:25 +0000

    afk

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ if not os.path.exists("db/"):
 start_time = time.time()
 
 
-
 def get_uptime():
     current_time = time.time()
     uptime_seconds = current_time - start_time
@@ -43,13 +42,20 @@ async def on_ready():
             "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, level INTEGER, coins INTEGER, afk INTEGER, xp INTEGER DEFAULT 0, xp_cooldown INTEGER DEFAULT 0, last_message_time REAL DEFAULT 0)"
         )
         await db.commit()
+        await db.execute(
+            "CREATE TABLE IF NOT EXISTS afk (guild_id INTEGER, user_id INTEGER, reason TEXT)"
+        )
+        await db.commit()
+
 
 def get_xp_for_next_level(level):
     # Simple formula for XP required for next level
     return 1000 * level
 
+
 # Cooldown period (in seconds)
 COOLDOWN_PERIOD = 120
+
 
 @bot.event
 async def on_message(message):
@@ -57,14 +63,19 @@ async def on_message(message):
         return
 
     async with aiosqlite.connect("./db/database.db") as db:
-        async with db.execute("SELECT xp, level, last_message_time FROM users WHERE id = ?", (message.author.id,)) as cursor:
+        async with db.execute(
+            "SELECT xp, level, last_message_time FROM users WHERE id = ?",
+            (message.author.id,),
+        ) as cursor:
             row = await cursor.fetchone()
             current_time = time.time()
 
             if row:
                 xp, level, last_message_time = row
                 if last_message_time is None:
-                    last_message_time = 0  # Default to 0 if last_message_time is not set
+                    last_message_time = (
+                        0  # Default to 0 if last_message_time is not set
+                    )
 
                 if current_time - last_message_time >= COOLDOWN_PERIOD:
                     xp_gain = random.randint(10, 40)
@@ -77,31 +88,62 @@ async def on_message(message):
                             embed=discord.Embed(
                                 title="Leveled Up!",
                                 description=f"Congratulations {message.author.mention}, you leveled up to level {level}!",
-                                color=0x4863A0
+                                color=0x4863A0,
                             ).set_footer(text="Made by Voidsudo"),
-                            delete_after=5
+                            delete_after=5,
                         )
                     await db.execute(
                         "UPDATE users SET xp = ?, level = ?, last_message_time = ? WHERE id = ?",
-                        (xp, level, current_time, message.author.id)
+                        (xp, level, current_time, message.author.id),
                     )
             else:
                 xp, level = random.randint(10, 40), 1
                 await db.execute(
                     "INSERT INTO users (id, xp, level, last_message_time) VALUES (?, ?, ?, ?)",
-                    (message.author.id, xp, level, current_time)
+                    (message.author.id, xp, level, current_time),
                 )
             await db.commit()
+
+    # check if afk
+    async with aiosqlite.connect("./db/database.db") as db:
+        async with db.execute(
+            "SELECT reason FROM afk WHERE user_id = ?", (message.author.id,)
+        ) as cursor:
+            row = await cursor.fetchone()
+            if row:
+                await db.execute(
+                    "DELETE FROM afk WHERE user_id = ?", (message.author.id,)
+                )
+                await db.commit()
+                await message.channel.send(
+                    f"{message.author.mention} You are no longer AFK."
+                )
+
+    # mention check for afk
+    if message.mentions:
+        for user in message.mentions:
+            async with aiosqlite.connect("./db/database.db") as db:
+                async with db.execute(
+                    "SELECT reason FROM afk WHERE user_id = ?", (user.id,)
+                ) as cursor:
+                    row = await cursor.fetchone()
+                    if row:
+                        await message.channel.send(
+                            f"{user.display_name} is AFK: {row[0]}"
+                        )
+
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
+
 
 @bot.event
 async def on_interaction(interaction):
     if interaction.guild_id is None:
         return await interaction.respond("This command can only be used in a server")
     await bot.process_application_commands(interaction)
+
 
 @bot.command(name="status", description="Show the bot's status and uptime.")
 async def status(ctx):
@@ -113,5 +155,6 @@ async def status(ctx):
     embed.set_thumbnail(url=bot.user.avatar.url)
     embed.set_footer(text="Made by Voidsudo")
     await ctx.respond(embed=embed)
+
 
 bot.run(TOKEN)
