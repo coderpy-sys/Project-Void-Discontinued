580dbcbc7d86fee3971cb7859a4fb98736bc5c4c
Voidsudo <109896528+voidsudo@users.noreply.github.com>
2024-07-01 12:00:05 +0800

    Cog loader

--- main.py
+++ main.py
@@ -93,9 +93,8 @@ async def on_error(event, *args, **kwargs):
             await channel.send(embed=embed)
 
 god = [1218756435664441404, 1129675180344610867, 1116705678745141339]
-cog = discord.SlashCommandGroup(name="cog", description="Cog commands")
 
-@cog.command()
+@bot.command()
 async def load(ctx, extension):
     if ctx.author.id in god:
         try:
@@ -113,7 +112,7 @@ async def load(ctx, extension):
         embed = loader_embed("Permission Denied", "You do not have permission to use this command.")
         await ctx.send(embed=embed, delete_after=5)
 
-@cog.command()
+@bot.command()
 async def unload(ctx, extension):
     if ctx.author.id in god:
         try:
@@ -131,7 +130,7 @@ async def unload(ctx, extension):
         embed = loader_embed("Permission Denied", "You do not have permission to use this command.")
         await ctx.send(embed=embed, delete_after=5)
 
-@cog.command()
+@bot.command()
 async def reload(ctx, extension):
     if ctx.author.id in god:
         try:
@@ -153,7 +152,7 @@ async def reload(ctx, extension):
         embed = loader_embed("Permission Denied", "You do not have permission to use this command.")
         await ctx.send(embed=embed, delete_after=5)
 
-@cog.command()
+@bot.command()
 async def list(ctx):
     embed = discord.Embed(title="Available Cogs", description="List of cogs currently available in the bot:", color=discord.Color.blue())
     for filename in os.listdir('./cogs'):
