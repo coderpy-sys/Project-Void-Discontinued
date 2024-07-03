95f8b58949541161017fb517655046609fdae8dd
Shadow <shadowstriker169@gmail.com>
2024-07-03 15:04:50 +0530

    Update ai.py

--- cogs/ai.py
+++ cogs/ai.py
@@ -1,36 +1,37 @@
 import discord
 from discord.ext import commands
-import sqlite3
-from rsnchat import RsnChat
+import aiosqlite
+from nextchat import NextChat
 
-rsn = RsnChat("rsnai_1MQfpO7qhSEvxVdb54sQMnnH")
-class GPT4(commands.Cog):
+rsn = NextChat("nextchat_f0l6u16v1zl80lkst3q5lhRq")
+
+class Ai(commands.Cog):
   
-  def __init__(self, bot):
+  def init(self, bot):
     self.bot = bot
-    self.db = sqlite3.connect("database.db")
+    self.db = aiosqlite.connect("database.db")
     self.cursor = self.db.cursor()
-    self.chatgpt = ChatGPT()
-
+    
   ai = SlashCommandGroup("Ai", "Ai related commands")
 
   @ai.command(name="setup", description="Set up a ChatBot channel")
-  async def gpt4_setup(self, ctx):
+  async def setup(self, ctx):
     channel = await ctx.guild.create_text_channel("chatbot")
-    self.cursor.execute("INSERT INTO channels (channel_id) VALUES (?)", (channel.id,))
-    self.db.commit()
-    await ctx.respond(f"A Channel has been created name <#{channel_id}>")
+    async with self.db.execute("INSERT INTO channels (channel_id) VALUES (?)", (channel.id,)) as cursor:
+    await self.db.commit()
+    await ctx.respond(f"A Channel has been created name <#{channel.id}>")
 
   @ai.command(name="chat", description="Chat with Chatbot")
-  async def gpt4_chat(self, ctx, message: str):
-    reply = await rsn.gpt(message)
+  async def chat(self, ctx, message: str):
+    reply = await rsn.gemini(message)
     await ctx.respond(reply['message'])
 
   @commands.cog.listener()
   async def on_message(self, message):
-    if message.channel.id in self.cursor.execute("SELECT channel_id FROM channels").fetchall():
-      reply = rsn.gpt(message.content)
-      await message.channel.send(response)
+    async with self.db.execute("SELECT channel_id FROM channels") as cursor:
+      if message.channel.id in [row[0] for row in await cursor.fetchall()]:
+        reply = await rsn.gemini(message.content)
+        await message.channel.send(reply['message'])
 
 def setup(bot):
   bot.add_cog(Ai(bot))
